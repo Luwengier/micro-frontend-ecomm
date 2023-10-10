6b02cb457561f6edad137f57178e8116abd59b19
Luewngier <g217881111@gmail.com>
2023-10-10 18:08:53 +0800

    Refactor cart so that no element is specified

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,16 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector('#dev-cart').innerHTML = cartText;
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+
+  el.innerHTML = cartText;
+}
+
+const el = document.querySelector('#dev-cart');
+
+if (process.env.NODE_ENV === 'development' && el) {
+  mount(el);
+}
+
+export default mount;
